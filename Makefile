0962b2f371b622dd63f1220357dd3ee7f7db77ee
BongoTech <c_mckiel@outlook.com>
2022-09-12 13:16:34 -0500

    Added driver.c and Makefile. Program compiles with a basic testing statement.

---
+++ Makefile
@@ -0,0 +1,16 @@
+CC 			= gcc			#default compiler
+CFLAGS 	= -g			#compilation flags
+TARGET 	= driver
+OBJS 		= driver.o loglib.o
+.SUFFIXES: .c .o
+
+.DEFAULT_GOAL : $(TARGET)
+$(TARGET) : $(OBJS)
+	$(CC) -o $@ $(OBJS)
+
+.c.o:
+	$(CC) $(CFLAGS) -c $<
+
+.PHONY : clean
+clean:
+	/bin/rm -f *.o $(TARGET)
---
+++ driver.c
@@ -0,0 +1,28 @@
+/**
+ *  @section INFO
+ *
+ *  @file driver.c
+ *  @author Cory Mckiel
+ *  @version 1.0
+ *  @date September 12, 2022 Created
+ *  @date September 12, 2022 Last modified
+ *
+ *  @section LICENSE 
+ *
+ *  This program currently has no licensing. Do with it what
+ *  you will.
+ *
+ *  @section DESCRIPTION
+ *  
+ *  driver.c is a program meant to prompt the user for log messages
+ *  and save them using the loglib library.
+ */
+
+#include <stdio.h>
+#include "log.h"
+
+int main(int argc, char *argv[]) {
+  printf("(testing) Return value of savelog: %d\n", savelog(NULL));
+  return 0;
+}
+
